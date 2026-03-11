6f75412977742b151317cdbc440d6fc2bf1b18aa
WYCO Nexus <55662767+WYCONexus@users.noreply.github.com>
2026-03-10 23:15:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -577,6 +577,24 @@ function setupTimelineLoadMore() {
     }
   }
 
+  /* ---------------------------
+   Workbench Image Lightbox
+--------------------------- */
+
+  const images = document.querySelectorAll(".workbench-img");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  images.forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
+    });
+  });
+
+  lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
 
   /* ---------------------------
      Initialize
