3b129f1a6b4d19b206dc19e57a8c9ff4c4a38dd2
WYCONexus <55662767+WYCONexus@users.noreply.github.com>
2026-03-13 15:12:42 -0500

    Update whimsy-data.js

--- js/whimsy-data.js
+++ js/whimsy-data.js
@@ -3,9 +3,10 @@ window.whimsyData = {
 
   featuredWriting: [
     {
-      type: "Book",
+      type: "text",
       title: "Nightmare",
       subtitle: "Clip · Chapter I",
+      badge: "Book",
       image: "/images/whimsy/nightmare.jpg",
       text: `
       In a lake of blood stood a young girl. Her lips taut; her eyes sliding into a frown down her face. The loss and sadness she was feeling echoed through the dormant winter trees. She inhaled, ripping cold air down her throat. She exhaled. Feeling the sorrow leave with every breath, she started to wade through the blood contaminated lagoon. Knee deep in animal blood and her heart broken, she looked up into the sky. Darkness rolled in quickly overhead, slowly enveloping the sunset. The reflected shine of light-less energy devouring clouds, cloud by cloud, mirrored off of her eyes. Pinks, oranges, and reds became nothing. A cessation of existence. Panicked, the girl started to scurry faster. Blood ripples getting closer together and more frequent in the water. Her white dress was now stained crimson, fading into an orange as it soaked into the fabric up her body. Spatters of the watered down life-force clung to her midsection. She approached the side of the lake and lifted her foot to the snow covered grass. Her feet were darkened by frostbite and her toes were hardened by the cold. She finished stepping up out of the lake and knelt down into the fluffy white sheet of ice. She turned around and laid back, her maroon colored hair, piercing into the white blanket of soft ice as her head hit the earth.
@@ -15,9 +16,10 @@ window.whimsyData = {
 
   poetryPassages: [
     {
-      type: "Free Verse",
+      type: "text",
       title: "We All Try",
       subtitle: "Poetry · WYCO",
+      badge: "Free Verse",
       image: "/images/whimsy/we-all-try.jpg",
       text: `
             Line 1
@@ -61,6 +63,7 @@ window.whimsyData = {
       type: "external",
       title: "Nightmare",
       subtitle: "Story Document",
+      badge: "Book",
       image: "/images/whimsy/nightmare.jpg",
       url: "https://docs.google.com/document/d/1raQGSbnho81BHXPuUgXpsD044l2HivExOG9HKMMDHYU/edit?usp=sharing"
     }
