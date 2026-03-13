13374914b4cbda5a934336d4ceff124695ab3b06
WYCO Nexus <55662767+WYCONexus@users.noreply.github.com>
2026-03-12 20:14:38 -0500

    Update waves-data.js

--- js/waves-data.js
+++ js/waves-data.js
@@ -1,22 +1,14 @@
 window.wavesData = {
   /*<!--FEATURED MUSIC-->*/
   featuredMusic: [
-    {
+    /*{
       title: "Blown Fuse",
       subtitle: "Single · WYCO Waves",
       badge: "Music",
       image: "/images/waves/wyco_gradient_diagonal.jpg",
       mediaSrc: "/audio/blown-fuse.mp3",
       mediaType: "audio"
-    },
-    {
-      title: "Drifting Sand",
-      subtitle: "Single · WYCO Waves",
-      badge: "Music",
-      image: "/images/waves/wyco_gradient_diagonal.jpg",
-      mediaSrc: "/audio/drifting-sand.mp3",
-      mediaType: "audio"
-    }
+    }*/
   ],
 
   /*<!--SHORT INSTRUMENTALS-->*/
